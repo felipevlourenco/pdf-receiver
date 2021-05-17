e43185fc8038f0b706982506d4e13ecac2f4ff74
Felipe Lourenco <felipe.v.lourenco@gmail.com>
2021-05-17 20:45:20 +0200

    Fix default port

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import logger from './logger/logger'
 dotenv.config({
   path: '.env'
 })
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 
 class Server {
   private _app = express()
